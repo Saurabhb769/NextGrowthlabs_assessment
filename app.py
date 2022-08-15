2185299c528449d0699932cc84e3c61f97ce97e0
Saurabhb769 <saurabhbansode80@gmail.com>
2022-08-15 12:15:06 +0530

    change

--- app.py
+++ app.py
@@ -7,28 +7,27 @@ from tensorflow import keras
 import numpy as np
 
 
-app = Flask(__name__,template_folder='template')
-model=keras.models.load_model('booking_hotel_chekedin.h5')
+app = Flask(__name__,template_folder="template")
+model=keras.models.load_model("booking_hotel_chekedin.h5")
 
-@app.route('/')
+@app.route("/")
 def home():
-	return render_template('index.html')sdff
+	return render_template("index.html")
 
-@app.route('/predict', methods=['POST'])
+@app.route("/predict", methods=["POST"])
 def predict():
-	'''
+	"""
 	for rendering results on HTML
-	'''	
-	print("-------------------------:",request.form.values())
+	"""	
 	feature = [float(x) for x in request.form.values()]
 	print(feature)
 	# re-arranging the list as per data set
-	feature_list = ['Age', 'DaysSinceCreation', 'AverageLeadTime', 'LodgingRevenue',
-       'OtherRevenue', 'PersonsNights', 'RoomNights', 'DistributionChannel',
-       'MarketSegment', 'SRHighFloor', 'SRLowFloor', 'SRAccessibleRoom',
-       'SRMediumFloor', 'SRBathtub', 'SRShower', 'SRCrib', 'SRKingSizeBed',
-       'SRTwinBed', 'SRNearElevator', 'SRAwayFromElevator',
-       'SRNoAlcoholInMiniBar', 'SRQuietRoom']
+	feature_list = ["Age", "DaysSinceCreation", "AverageLeadTime", "LodgingRevenue",
+       "OtherRevenue", "PersonsNights", "RoomNights", "DistributionChannel",
+       "MarketSegment", "SRHighFloor", "SRLowFloor", "SRAccessibleRoom",
+       "SRMediumFloor", "SRBathtub", "SRShower", "SRCrib", "SRKingSizeBed",
+       "SRTwinBed", "SRNearElevator", "SRAwayFromElevator",
+       "SRNoAlcoholInMiniBar", "SRQuietRoom"]
 	
 	
 	prediction = model.predict(np.array(feature).reshape(1,-1))
@@ -42,10 +41,10 @@ def predict():
 	else:
 		result = "CHEKED IN"
 
-	return  result# render_template('index.html', prediction_text = result)
+	return  result# render_template("index.html", prediction_text = result)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 	app.run(debug=True)
 
 
