8185c7794f5269c7f1c6f34dbf80e564f30b0479
Saurabhb769 <saurabhbansode80@gmail.com>
2022-08-15 11:39:28 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,51 @@
+
+
+import pickle
+from flask import Flask,request,jsonify,render_template
+
+from tensorflow import keras
+import numpy as np
+
+
+app = Flask(__name__,template_folder='template')
+model=keras.models.load_model('booking_hotel_chekedin.h5')
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+	'''
+	for rendering results on HTML
+	'''	
+	print("-------------------------:",request.form.values())
+	feature = [float(x) for x in request.form.values()]
+	print(feature)
+	# re-arranging the list as per data set
+	feature_list = ['Age', 'DaysSinceCreation', 'AverageLeadTime', 'LodgingRevenue',
+       'OtherRevenue', 'PersonsNights', 'RoomNights', 'DistributionChannel',
+       'MarketSegment', 'SRHighFloor', 'SRLowFloor', 'SRAccessibleRoom',
+       'SRMediumFloor', 'SRBathtub', 'SRShower', 'SRCrib', 'SRKingSizeBed',
+       'SRTwinBed', 'SRNearElevator', 'SRAwayFromElevator',
+       'SRNoAlcoholInMiniBar', 'SRQuietRoom']
+	
+	
+	prediction = model.predict(np.array(feature).reshape(1,-1))
+
+	
+	print("prediction value: ", prediction)
+
+	result = ""
+	if prediction == 0:
+		result = "NOT CHEKED IN"
+	else:
+		result = "CHEKED IN"
+
+	return  result# render_template('index.html', prediction_text = result)
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
+
+
